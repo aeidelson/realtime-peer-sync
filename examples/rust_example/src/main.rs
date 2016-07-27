130758e85eacaaa76750801c186ef91d8bc428c4
Aaron Eidelson <aeidelson@dropbox.com>
2016-07-27 15:16:28 -0700

    Work on simple drawing in exaple piston app

--- examples/rust_example/src/main.rs
+++ examples/rust_example/src/main.rs
@@ -7,6 +7,8 @@ use std::thread;
 use piston_window::{
     PistonWindow,
     WindowSettings,
+    Event,
+    MouseCursorEvent,
 };
 
 use RPS::{client, common, server};
@@ -29,21 +31,39 @@ fn main() {
 
     server.start().unwrap();
 
+    let mut obj_locations: Vec<(f64, f64)> = Vec::new();
+
     // Start piston to draw and handle user input.
     let mut window: PistonWindow =
         WindowSettings::new("Hello Piston!", [640, 480])
             .exit_on_esc(true).build().unwrap();
-    while let Some(e) = window.next() {
-        window.draw_2d(&e, |c, g| {
-            // Note: The client can be accessed here and is borrowed.
-            let _ = client.user_interaction_started();
-            //client.shutdown();
 
-            piston_window::clear([1.0; 4], g);
-            piston_window::rectangle([1.0, 0.0, 0.0, 1.0], // red
-                                      [0.0, 0.0, 100.0, 100.0],
-                                      c.transform, g);
-        });
+    while let Some(e) = window.next() {
+        let borrowed_locations = &mut obj_locations;
+
+        // Note(aeidelson): I was having a lot of trouble gating this on clicks. It doesn't seem
+        // like clicks and cursor locations are passed in the same event?
+        if let Some(cursor_location) = e.mouse_cursor_args() {
+            borrowed_locations.push((cursor_location[0], cursor_location[1]));
+        }
+
+        if let Event::Render(_) = e {
+            window.draw_2d(&e, |c, g| {
+                // Note: The client can be accessed here and is borrowed.
+                // i.e.: let _ = client.user_interaction_started();
+
+                piston_window::clear([1.0; 4], g);
+
+                for &mut (x, y) in borrowed_locations {
+                    piston_window::rectangle(
+                        [1.0, 0.0, 0.0, 1.0], // red
+                        [x - 5.0, y - 5.0, 10.0, 10.0],
+                        c.transform,
+                        g,
+                    );
+                }
+            });
+        }
     }
     
     server.shutdown();
